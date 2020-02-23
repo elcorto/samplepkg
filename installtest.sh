a7361ab9d24ccc10c1909cb09fc2dce9fa406df0
Steve Schmerler <git@elcorto.com>
2020-02-24 00:25:00 +0100

    Move all env var unsetting to uninstall()
    Also use uninstall() inside the subshell in which we run $cmd .

--- installtest.sh
+++ installtest.sh
@@ -2,12 +2,25 @@
 
 set -eu
 
+err(){
+    echo "error: $@"
+    exit 1
+}
+
+
 here=$(pwd)
+[ -d src ] || err "no src/ found"
 pkgname=$(basename $(ls $here/src))
 
+log=installtest.log
+
+# Adapt to your system.
 version=python3.7
 venv=__test_venv__
-log=installtest.log
+user_prefix=$HOME/soft
+user_venv=$HOME/$venv/
+
+local_prefix=$HOME/.local
 
 # Seems like that there is no way to force pipenv to use a specific venv. No,
 # PIPENV_VIRTUALENV is ignored. We can only use
@@ -15,12 +28,9 @@ log=installtest.log
 # to at least enforce $(pwd)/.venv .
 pipenv_venv=$(dirname $0)/.venv
 
-user_prefix=$HOME/soft
-user_venv=$HOME/$venv/
-
 user_site_pkg=$user_prefix/lib/$version/site-packages/
 user_venv_site_pkg=$user_venv/lib/$version/site-packages/
-local_site_pkg=$HOME/.local/lib/$version/site-packages/
+local_site_pkg=$local_prefix/lib/$version/site-packages/
 pipenv_venv_site_pkg=$pipenv_venv/lib/$version/site-packages/
 
 user_egg_link=$user_site_pkg/${pkgname}.egg-link
@@ -39,12 +49,6 @@ pipenv_venv_easy_install_pth=$pipenv_venv_site_pkg/easy-install.pth
 pkg_rex="dummy.+test|$pkgname"
 
 
-err(){
-    echo "error: $@"
-    exit 1
-}
-
-
 uninstall(){
     echo "uninstall" >> $log
     # pip search -> dummy_test, but after install: pip list ->
@@ -55,9 +59,11 @@ uninstall(){
         rm -rfv $user_site_pkg/$name* $local_site_pkg/$name* >> $log 2>&1
     done
     rm -rfv $user_venv >> $log 2>&1
-    rm -rfv $pipenv_venv >> $log 2>&1
-    $here/clean.sh
+    rm -rfv $pipenv_venv Pipfile* >> $log 2>&1
     unset $(env | grep PIPENV_ | sed -re 's/^(PIPENV_.+)=.+/\1/g')
+    [ -z "$(env | grep PIPENV_)" ] || err "unset env vars PIPENV_* failed"
+    unset PYTHONUSERBASE PYTHONPATH
+    [ -z "$(env | grep PYTHON)" ] || err "unset env vars PYTHON* failed"
 }
 
 
@@ -82,13 +88,9 @@ greppkg(){
 
 run(){
     cmd=$@
-    uninstall
     echo "\n$cmd" | sed -re 's|//|/|g' | tee -a $log
     (
-    unset PYTHONUSERBASE PYTHONPATH
-    [ -z "$(env | grep PYTHON)" ] || err "unset env vars PYTHON* failed"
-    unset $(env | grep PIPENV_ | sed -re 's/^(PIPENV_.+)=.+/\1/g')
-    [ -z "$(env | grep PIPENV_)" ] || err "unset env vars PIPENV_* failed"
+    uninstall
     eval "$cmd >> $log 2>&1"
     cat << eof
     which pip3 : $(which pip3)
@@ -125,16 +127,22 @@ run "pip3 install -e ."
 run "PYTHONUSERBASE=$user_prefix pip3 install -e ."
 run "PYTHONPATH=$user_site_pkg python3 setup.py develop --prefix=$user_prefix"
 
+# plain ol' Python venv, all other venv-ish tools/wrappers use this under the
+# hood (right?, else PRs welcome!), so we treat only this here
 run "$version -m venv --without-pip --symlinks $user_venv; . $user_venv/bin/activate; pip3 install ."
 run "$version -m venv --symlinks $user_venv; . $user_venv/bin/activate; pip3 install ."
 
+# pipenv: replace ". $pipenv_venv/bin/activate" w/ "pipenv shell" in interactive
+# work
+
+run "PIPENV_VENV_IN_PROJECT=1 pipenv install . >> $log 2>&1; . $pipenv_venv/bin/activate"
+run "PIPENV_VENV_IN_PROJECT=1 pipenv install -e . >> $log 2>&1; . $pipenv_venv/bin/activate"
+
+# same as the ones below
 run "PIPENV_VENV_IN_PROJECT=1 pipenv install >> $log 2>&1; . $pipenv_venv/bin/activate; pip3 install ."
 run "PIPENV_VENV_IN_PROJECT=1 pipenv install >> $log 2>&1; . $pipenv_venv/bin/activate; pip3 install -e ."
 
 run "(PIPENV_VENV_IN_PROJECT=1 pipenv install && pipenv run pip install .) >> $log 2>&1; . $pipenv_venv/bin/activate"
 run "(PIPENV_VENV_IN_PROJECT=1 pipenv install && pipenv run pip install -e .) >> $log 2>&1; . $pipenv_venv/bin/activate"
-
-run "PIPENV_VENV_IN_PROJECT=1 pipenv install . >> $log 2>&1; . $pipenv_venv/bin/activate"
-run "PIPENV_VENV_IN_PROJECT=1 pipenv install -e . >> $log 2>&1; . $pipenv_venv/bin/activate"
 
 uninstall
